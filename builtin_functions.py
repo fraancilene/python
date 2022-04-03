525ccd3e4526d66f2e433b69928fa13cf559629d
Francilene Silva <francilenesilva.fps10@gmail.com>
2022-04-03 23:01:27 +0100

    Itrodução python

---
+++ builtin_functions.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# import de bibliotecas que não são nativas - precisa instalar o modulo primeiramente
+import math #importa todo o modulo
+from math import sqrt, cos # importa apenas a função que precisamos
+from math import * # importando todas as funções de dentro do pacote math
+
+
+x = {}
+y = ()
+z = []
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# help(np.random.random())
+
+print(math.sqrt(25));
+print(sqrt(81));
+print(cos(6));
+print(floor(6.78)) # arredonda para baixo
+
+
+'''
+    para instalar bibliotecas externas
+    pip3 install nome_do_modulo
+'''
+
---
+++ hello.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+print("Hello world");
+print (3+2);
+# comentário em python de uma linha
+''' 
+    comentário em mais de uma 
+    linha em python
+
+'''
+
+# indentação é automática e obrigatória
+for i in range(10):
+    print (i);
+    
+def fun(a, b):
+    return a + b;
+
+
